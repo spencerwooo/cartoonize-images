6539d4c1f63d189cb3c98f382f79443904f3ef89
spencerwooo <spencerwoo98@foxmail.com>
2019-01-30 22:45:27 +0800

    Add main function

--- main.m
+++ main.m
@@ -1,20 +1,22 @@
-img = imread('image.jpg');
+imgPath = 'image2.jpg';
 
-figure(1)
-imshow(img);
+saturation = 1;
 
-saturation = 2;
+edgeThreshhold = 0.2;
+edgeDetector = 'canny';
 
-edgeThreshhold = 0.02;
-edgeDetector = 'sobel';
+sigma = 5;
 
-spatialSigma = 2;
+img = imread(imgPath);
 
-% 1. 提升饱和度
+% figure(1)
+% imshow(img);
+
+% 1. Saturate
 % Add a degree of saturation, to make image colors more vibrant
 imgSaturated = saturateImage(img, saturation);
 
-% 2. 弱化细节（区域平滑)
+% 2. Bilateral filter
 % Convert the image to the L*a*b colorspace
 imgLAB = rgb2lab(imgSaturated);
 % Extract an L*a*b patch that contains no sharp edges
@@ -24,11 +26,11 @@ edist = sqrt(sum(patchSq, 3));
 patchVar = std2(edist).^2;
 % Filter image with bilateral filtering
 smoothness = patchVar * 4;
-smoothedLABImg = imbilatfilt(imgLAB, smoothness, spatialSigma);
+smoothedLABImg = imbilatfilt(imgLAB, smoothness, sigma);
 % Convert the image back to the RGB color space
 smoothedRBGImg = lab2rgb(smoothedLABImg, 'Out', 'uint8');
 
-% 3. 突出边缘线条
+% 3. Highlight edges
 imgGray = rgb2gray(smoothedRBGImg);
 edgeMask = uint8(edge(imgGray, edgeDetector, edgeThreshhold));
 
@@ -37,8 +39,8 @@ resultImg(:,:,1) = smoothedRBGImg(:,:,1) - smoothedRBGImg(:,:,1) .* edgeMask;
 resultImg(:,:,2) = smoothedRBGImg(:,:,2) - smoothedRBGImg(:,:,2) .* edgeMask;
 resultImg(:,:,3) = smoothedRBGImg(:,:,3) - smoothedRBGImg(:,:,3) .* edgeMask;
 
-% 显示图像
-% montage({img, resultImg})
-% title('Original Image vs. Filtered Image');
-figure(2)
-imshow(resultImg);
+% Display result
+montage({img, resultImg})
+title('Original Image vs. Filtered Image');
+% figure(2)
+% imshow(resultImg);
